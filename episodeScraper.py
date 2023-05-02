5d7ff8e66eed264087b2745990992e2b3895801e
deboradum <pvwpvw@protonmail.com>
2023-05-02 18:24:51 +0200

    renamed file

--- episodeScraper.py
+++ episodeScraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-links = {'S02E08': 'https://southpark.fandom.com/wiki/Summer_Sucks/Script',
+urls = {'S02E08': 'https://southpark.fandom.com/wiki/Summer_Sucks/Script',
          'S02E10': 'https://southpark.fandom.com/wiki/Chickenpox/Script',
          'S02E12': 'https://southpark.fandom.com/wiki/Clubhouses/Script',
          'S03E06': 'https://southpark.fandom.com/wiki/Sexual_Harassment_Panda/Script',
@@ -12,7 +12,16 @@ links = {'S02E08': 'https://southpark.fandom.com/wiki/Summer_Sucks/Script',
          'S07E14': 'https://southpark.fandom.com/wiki/Raisins/Script',
          'S09E10': 'https://southpark.fandom.com/wiki/Follow_That_Egg!/Script',
          'S10E06': 'https://southpark.fandom.com/wiki/ManBearPig/Script',
-         '': '',
-         '': '',
-         '': ''
-        }
+         'S12E13': 'https://southpark.fandom.com/wiki/Elementary_School_Musical/Script',
+         'S13E02': 'https://southpark.fandom.com/wiki/The_Coon/Script',
+         'S13E12': 'https://southpark.fandom.com/wiki/The_F_Word/Script',
+         'S14E06': 'https://southpark.fandom.com/wiki/201/Script',
+         'S14E09': 'https://southpark.fandom.com/wiki/It%27s_a_Jersey_Thing/Script',
+         'S15E03': 'https://southpark.fandom.com/wiki/Royal_Pudding/Script',
+         'S20E19': 'https://southpark.fandom.com/wiki/Not_Funny/Script',
+         'S21E07': 'https://southpark.fandom.com/wiki/Doubling_Down/Script',
+         'S22E03': 'https://southpark.fandom.com/wiki/The_Problem_with_a_Poo/Script'}
+
+
+for url in urls:
+    pass
