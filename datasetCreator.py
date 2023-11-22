a346da072160cf832ca51a4ea84fa9b40773ad89
deboradum <pvwpvw@protonmail.com>
2023-11-22 16:33:22 +0900

    Created parse script

--- datasetCreator.py
+++ datasetCreator.py
@@ -1,24 +1,23 @@
 import os
 import json
+for season in range(1,27):
+    for episode in range(1, 27):
+        data = []
+        try:
+            with open(f"scripts/S{season:02}/S{season:02}E{episode:02}.txt", "r") as f:
+                currItem = {
+                    "input": "", 
+                    "output": ""
+                }
+                for line in f:
+                    if line.isupper():
+                        data.append(currItem.copy())
+                        currItem["input"] = currItem["output"]
+                        currItem["output"] = ""
+                    else:
+                        currItem["output"] += line
 
-data = []
-prevText = ""
-newText = ""
-
-with open("scripts/S01/S01E01.txt") as f:
-    currItem = {
-        "input": "", 
-        "output": ""
-    }
-    nextItem = {
-        "input": "", 
-        "output": ""
-    }
-    for line in f:
-        if line.isupper():
-            data.append(currItem)
-            currItem["output"] += line
-            nextItem["input"] += line
-        else:
-            currItem["output"] += line
-            nextItem["input"] += line
+            with open(f"parsed/S{season:02}/S{season:02}E{episode:02}.json", "w+") as f:
+                f.write(json.dumps(data, indent=2))
+        except Exception as e:
+            print(f"Error converting season {season} - Episode {episode}")
