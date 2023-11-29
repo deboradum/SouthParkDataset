09ae651c6e3372a76f8e4892ff43cd8af85ae990
deboradum <pvwpvw@protonmail.com>
2023-11-29 14:40:08 +0900

    Added context in dataset

--- datasetCreator.py
+++ datasetCreator.py
@@ -1,34 +1,55 @@
 import os
 import json
 
+def append_context(context, new_item, max_context=10):
+    if len(context) >= max_context:
+        del context[0]
+    context.append(new_item)
+
+    return context
+
+def context_to_string(context):
+    string = ""
+    for l in context:
+        string += l
+
+    return string
+
 dataAll = []
 for season in range(1,27):
     dataSeason = []
-    for episode in range(1, 27):
+    for episode in range(1, 19):
         data = []
         try:
             with open(f"scripts/S{season:02}/S{season:02}E{episode:02}.txt", "r") as f:
                 currItem = {
                     "input": "", 
-                    "output": ""
+                    "output": "",
+                    "context": "",
                 }
+                context = []
                 for line in f:
                     if line.isupper():
                         data.append(currItem.copy())
                         dataSeason.append(currItem.copy())
                         dataAll.append(currItem.copy())
                         currItem["input"] = currItem["output"]
+                        currItem["context"] = context_to_string(append_context(
+                                                                    context, 
+                                                                    currItem["output"], 
+                                                                    max_context=30)
+                                                                )
                         currItem["output"] = ""
                     else:
                         currItem["output"] += line
 
-            with open(f"parsed/S{season:02}/S{season:02}E{episode:02}.json", "w+") as f:
+            with open(f"parsed_context_30/S{season:02}/S{season:02}E{episode:02}.json", "w+") as f:
                 f.write(json.dumps(data, indent=2))
         except Exception as e:
-            print(f"Error converting season {season} - Episode {episode}")
+            print(f"Error converting season {season} - Episode {episode}", e)
 
-        with open(f"parsed/S{season:02}/S{season:02}.json", "w+") as f:
+        with open(f"parsed_context_30/S{season:02}/S{season:02}.json", "w+") as f:
                 f.write(json.dumps(dataSeason, indent=2))
 
-with open(f"parsed/all.json", "w+") as f:
+with open(f"parsed_context_30/all.json", "w+") as f:
         f.write(json.dumps(dataAll, indent=2))
