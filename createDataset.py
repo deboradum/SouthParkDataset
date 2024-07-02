85c50e0d848df5f193498ea341a1cf2ad06d27e4
deboradum <pvwpvw@protonmail.com>
2024-07-02 18:28:23 +0200

    convo size dataset generation

--- createDataset.py
+++ createDataset.py
@@ -8,9 +8,6 @@ SYSTEM = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>"
 USER = "<|start_header_id|>user<|end_header_id|>"
 ASSISTENT = "<|start_header_id|>assistant<|end_header_id|>"
 
-CONVO_CONTEXT = 10
-
-
 def generate_system_prompt(prompt):
     return f"{SYSTEM}\n\n{prompt}<|eot_id|>\n"
 
@@ -23,23 +20,16 @@ def generate_speaker_prompt(speaker, prompt):
     return f"{get_speaker_header(speaker)}\n\n{prompt}<|eot_id|>\n"
 
 
-def append_to_history(history, new, max_length=CONVO_CONTEXT):
-    if len(history) < max_length:
-        history.append(new)
-    else:
-        history = history[1:]
-        history.append(new)
-
-    return history
-
 def has_alphabetical_characters(input_string):
     return bool(re.search(r'[a-zA-Z]', input_string))
 
-def parse_episode_convo_window(season, episode):
+def parse_episode_convo_window(season, episode, convo_size):
     first = True
     history = []
     speaker = None
     speakerText = ""
+    if not os.path.isdir(f"data_{convo_size}"):
+        os.mkdir(f"data_{convo_size}")
     with open(f"scripts/{season}/{episode}", "r") as f:
         for line in f:
             if line.startswith("[") and line.endswith("]\n"):
@@ -48,18 +38,27 @@ def parse_episode_convo_window(season, episode):
                 continue
 
             if first:
-                history = append_to_history(
-                    history, generate_system_prompt(line.replace("\n", "").strip())
-                )
+                history.append(generate_system_prompt(line.replace("\n", "").strip()))
                 first = False
                 continue
 
             # Finds speaker
             if line.isupper():
                 if speakerText:
-                    history = append_to_history(history, generate_speaker_prompt(speaker, speakerText))
-                    with open(f"parsed/{season}/{episode.replace('.txt','')}.jsonl", "a+") as output_f:
-                        output_f.write(json.dumps({"text": "".join(history)}) + "\n")
+                    if len(history) == convo_size:
+                        num = random.random()
+                        if num < 0.7:
+                            with open(f"data_{convo_size}/train.jsonl", "a+") as output_f:
+                                output_f.write(json.dumps({"text": "".join(history)}) + "\n")
+                        elif num < 0.85:
+                            with open(f"data_{convo_size}/valid.jsonl", "a+") as output_f:
+                                output_f.write(json.dumps({"text": "".join(history)}) + "\n")
+                        else:
+                            with open(f"data_{convo_size}/test.jsonl", "a+") as output_f:
+                                output_f.write(json.dumps({"text": "".join(history)}) + "\n")
+                        history = []
+                    else:
+                        history.append(generate_speaker_prompt(speaker, speakerText))
 
                 speaker = line.replace("\n", "").strip()
                 speakerText = ""
@@ -94,19 +93,23 @@ def parse_episode_full(season, episode):
                 speakerText += line
 
     num = random.random()
-    if num < 0.75:
-        with open("train.jsonl", "a+") as output_f:
+    if num < 0.7:
+        with open("data/train.jsonl", "a+") as output_f:
+            output_f.write(json.dumps({"text": "".join(history)}) + "\n")
+    elif num < 0.85:
+        with open("data/valid.jsonl", "a+") as output_f:
             output_f.write(json.dumps({"text": "".join(history)}) + "\n")
     else:
-        with open("test.jsonl", "a+") as output_f:
+        with open("data/test.jsonl", "a+") as output_f:
             output_f.write(json.dumps({"text": "".join(history)}) + "\n")
 
 if __name__ == "__main__":
+    convo_size = 10
     for season in os.listdir("scripts/"):
         if season.startswith("."):
             continue
         for episode in os.listdir(f"scripts/{season}"):
             if episode.startswith("."):
                 continue
-            parse_episode_full(season, episode)
-            # exit()
+            # parse_episode_full(season, episode)
+            parse_episode_convo_window(season, episode, convo_size)
