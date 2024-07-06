6a251b9ecaa6c79f996c99654af752503896eccb
deboradum <pvwpvw@protonmail.com>
2024-07-06 16:45:19 +0200

    llm response filtering and writing every generation

--- generateEpisode.py
+++ generateEpisode.py
@@ -29,21 +29,29 @@ def response_to_monolog(text):
 
     return None
 
+def filter_non_standard_characters(text):
+    pattern = re.compile(r'[^\w\s.,:;!?\'"@#%&*()\-+=/\\[\]{}<>|`~^]', re.UNICODE)
+    cleaned_string = pattern.sub('', text)
+
+    return cleaned_string
+
 
 class EpisodeGenerator:
-    def __init__(self, model, adapter_path):
+    def __init__(self, model, adapter_path, script_filepath):
         self.model, self.tokenizer = load(model, adapter_path=adapter_path)
         self.script = []
+        self.filepath = script_filepath
 
-    def generate_next_dialog(self, dialog, max_tokens=200, temp=0.0):
+    def generate_next_dialog(self, dialog, max_tokens=100, temp=0.3):
         return generate(
             self.model,
             self.tokenizer,
-            dialog_to_prompt(self.script[-4:]),
+            dialog_to_prompt(dialog),
             max_tokens,
             verbose=True,
             temp=temp,
-            repetition_penalty=2.0,
+            # repetition_penalty=1.5,
+            # repetition_context_size=200,
         )
 
     # initial_dialog = [{"speaker": <speaker>, "text": <text>}, ...]
@@ -53,26 +61,41 @@ class EpisodeGenerator:
         self, episode_title, initial_dialog=[], episode_length=100
     ):
         self.script = []
-        self.script.append({"speaker": "system", "text": episode_title})
+        system_prompt = {"speaker": "system", "text": episode_title}
+        self.script.append(system_prompt)
+        self.write_monolog_to_file(system_prompt)
 
         # Sets up initial dialog
         for monolog in initial_dialog:
             self.script.append(monolog)
-        for _ in range(episode_length):
+            self.write_monolog_to_file(monolog)
 
+        for _ in range(episode_length):
             response = self.generate_next_dialog(self.script[-4:])
-            monolog = response_to_monolog(response)
+            filtered_response = filter_non_standard_characters(response)
+            monolog = response_to_monolog(filtered_response)
             if monolog:
                 self.script.append(monolog)
+                self.write_monolog_to_file(monolog)
+            else:
+                print("Error: Could not convert the following response to a monolog object:")
+                print("---------")
+                print(filtered_response)
 
 
-    def write_to_file(self, filepath):
+    def write_monolog_to_file(self, monolog):
+        with open(self.filepath, "a+") as f:
+            json.dump(monolog, f)
+            f.write("\n")
+
+
+    def write_script_to_file(self, filepath):
         with open(filepath, "w") as f:
             json.dump(self.script, f)
 
 
 if __name__ == "__main__":
-    generator = EpisodeGenerator("meta-llama/Meta-Llama-3-8B-Instruct", "adapters")
+    generator = EpisodeGenerator("meta-llama/Meta-Llama-3-8B-Instruct", "adapters", "michaelJackson.jsonl")
     title = "Michael Jackson loves little boys"
     initial_dialog = [
         {"speaker": "CARTMAN", "text": "Holy shit guys! I just got a text message from Michael jackson. He wants to see me!"},
